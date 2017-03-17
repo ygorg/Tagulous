716fe4c1176576d4be140688866d63990204a7ab
ra1nbowpill <ygor.gallina@gmail.com>
2017-03-18 00:07:55 +0100

    Created TagListWidget class
    It regroups the code to show a list of Tag

---
+++ taglistwidget.cpp
@@ -0,0 +1,111 @@
+#include "taglistwidget.h"
+#include <QDebug>
+
+TagListWidget::TagListWidget(TagListModel *tagListModel,
+                             QMap<QString, QAction *> *actions,
+                             QWidget *parent)
+    : QWidget(parent) {
+
+    /* Initialize the widgets and variables */
+    _actions = actions;
+    _tagListModel = tagListModel;
+    _layout = new QVBoxLayout;
+    _tagView = new QListView;
+    _searchBox = new QLineEdit(tr("Search"));
+
+    _tagView->setModel(_tagListModel);
+    _tagView->setSelectionMode(QAbstractItemView::ExtendedSelection);
+    _tagView->setDragEnabled(true);
+    _tagView->setAcceptDrops(true);
+    _tagView->setDropIndicatorShown(true);
+    _tagView->setAttribute(Qt::WA_MacShowFocusRect, false);
+    _tagView->setEditTriggers(QAbstractItemView::SelectedClicked);
+
+    /* Defining the layout */
+    _layout->addWidget(_searchBox);
+    _layout->addWidget(_tagView);
+    this->setLayout(_layout);
+}
+
+
+void TagListWidget::connectActions() {
+
+    /* Connect the wanted action to the wanted slot */
+    /* Tell if you want to action to be shown in the toolbar
+     * and/or the menu */
+
+    connect(_actions->value("add"), SIGNAL(triggered(bool)),
+            _tagListModel, SLOT(requestedAddTag()));
+    emit toolBarAction("add");
+    emit menuAction("add");
+
+
+    connect(_actions->value("copy"), SIGNAL(triggered(bool)),
+            this, SLOT(copyElement()));
+    emit menuAction("copy");
+
+
+    connect(_actions->value("paste"), SIGNAL(triggered(bool)),
+            this, SLOT(pasteElement()));
+    emit menuAction("paste");
+
+
+    connect(_actions->value("remove"), SIGNAL(triggered(bool)),
+            this, SLOT(deleteElement()));
+    emit toolBarAction("remove");
+    emit menuAction("remove");
+
+
+    connect(_actions->value("rename"), SIGNAL(triggered(bool)),
+            this, SLOT(renameElement()));
+    emit toolBarAction("rename");
+    emit menuAction("rename");
+
+
+    connect(_actions->value("filter"), SIGNAL(triggered(bool)),
+            this, SLOT(filterTags()));
+    emit toolBarAction("filter");
+    emit menuAction("filter");
+
+}
+
+void TagListWidget::deleteElement() {
+    /* Removing element from last to first as we operate on a list
+     * removing the first element moves all the other...
+     */
+    QModelIndexList indexes = _tagView->selectionModel()->selectedRows();
+    QModelIndexList::reverse_iterator it;
+
+    for(it = indexes.rbegin(); it != indexes.rend(); ++it) {
+        //delete tagList->at(index.row());
+        _tagListModel->removeRow(it->row(), it->parent());
+    }
+}
+
+void TagListWidget::renameElement() {
+    if (_tagView->selectionModel()->selectedRows().length() <= 0) {
+        return;
+    }
+    /* Editing the first item if more than one is selected */
+    _tagView->edit(_tagView->selectionModel()->selectedRows().at(0));
+}
+
+void TagListWidget::copyElement() {
+    qDebug() << "Copy on tags to be implemented";
+}
+void TagListWidget::pasteElement() {
+    qDebug() << "Paste on tags to be implemented";
+}
+
+void TagListWidget::filterTags() {
+    //TODO here add checkboxes in front of the tags
+    qDebug() << "Multi selection to be implemented";
+}
+
+void TagListWidget::doubleClicked(QModelIndex index) {
+    if (!index.isValid()) {
+        return;
+    }
+    /* We want to change view so we SIGNAL it */
+    emit viewFileList(index.row());
+}
---
+++ taglistwidget.h
@@ -0,0 +1,47 @@
+#ifndef TAGLISTWIDGET_H
+#define TAGLISTWIDGET_H
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QListView>
+#include <QLineEdit>
+#include <QMenuBar>
+#include <QAction>
+#include <QMap>
+
+#include <QAbstractItemModel>
+
+#include "taglistmodel.h"
+
+class TagListWidget : public QWidget
+{
+    Q_OBJECT
+private:
+    QMap<QString, QAction *> *_actions;
+
+    QVBoxLayout *_layout;
+
+    QLineEdit *_searchBox;
+    QListView *_tagView;
+
+    QAbstractItemModel *_tagListModel;
+public:
+    explicit TagListWidget(TagListModel *, QMap<QString, QAction *> *, QWidget *parent=0);
+
+    void connectActions();
+
+signals:
+    void toolBarAction(QString);
+    void menuAction(QString);
+    void viewFileList(int);
+
+public slots:
+    void deleteElement();
+    void copyElement();
+    void pasteElement();
+    void filterTags();
+    void renameElement();
+    void doubleClicked(QModelIndex);
+};
+
+#endif // TAGLISTWIDGET_H
